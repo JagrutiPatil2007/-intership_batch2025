3060607b5651871f9cb7db5b6543cda92a8b4372
JagrutiPatil2007 <jagrutipatil8238@gmail.com>
2025-06-06 17:00:16 +0530

    hello

---
+++ java/day2/demo.java
@@ -0,0 +1,14 @@
+package sample.java;
+
+public class demo {
+
+	public static void main(String[] args) {
+		int a=10;
+		System.out.println("Enter the 1 number:-"+a);
+		int b=20;
+		System.out.println("Enter the 2 number:-"+b);
+		int c=a+b;
+		System.out.println("sum is:-"+c);
+	}
+}
+	    
