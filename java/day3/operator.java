c7270d4360f9c250d207d0ae86ad87e41cf19a92
JagrutiPatil2007 <jagrutipatil8238@gmail.com>
2025-06-10 15:51:50 +0530

    day 3

---
+++ java/day3/operator.java
@@ -0,0 +1,28 @@
+package relational.java;
+
+import java.util.Scanner;
+
+public class operator {
+
+	public static void main(String[] args) {
+		        System.out.println("Enter the Integer:");
+	            Scanner s=new Scanner(System.in);
+                int num=s.nextInt();
+		        if (num > 0) 
+		        {
+		            System.out.println("The number is Positive.");
+		        } 
+		        else if (num < 0) 
+		        {
+		            System.out.println("The number is Negative.");
+		        } 
+		        else 
+		        {
+		            System.out.println("The number is Zero.");
+		        }
+		    
+		
+
+   	}
+
+}
